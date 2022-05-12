a752f7d72249e4fbf28635ddb9aee6d046f5e746
Renato Nakamura <65171479+Renato-Nakamura@users.noreply.github.com>
2022-05-12 16:53:23 -0300

    :sparkles: (screen): added homepage and logo

---
+++ components/Logo.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col items-end w-55">
+    <span class="mt-[-.5rem] text-5xl">HomeStats</span>
+    <span class="mt-[-.4rem] text-lg">By Renato Nakamura</span>
+  </div>
+</template>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,32 @@
 <template>
-    <div>
-        <header class="">
-            <h1 @click="teste++" class="text-sm text-gray-500">Landing Page {{teste}}</h1>
-            <button class="bg-sky-500 rounded p-1 text-white font-medium"  @click="login">Login com Google</button>
-        </header>
-        <nuxt-link to="upload-file"> Subir arquivo</nuxt-link>
-    </div>
+  <div>
+    <header class="flex justify-between px-10 py-5">
+      <Logo />
+      <div class="flex items-center text-lg">
+          <span class="mx-1">LinkedIn</span> 
+          <span class="mx-1">Sobre</span>
+      </div>
+    </header>
+    <main>
+        <section class="flex justify-center">
+            <div class="w-5/12 px-20 py-10 gap-4 flex flex-col">
+                <h1 class="text-5xl font-bold">Entender suas finanças nunca foi tão fácil</h1>
+                <p>Com HomeStats você consegue visualizar os números e entender cada ponto.
+                Nunca mais encare uma planilha por horas, envie o seu csv para o HomeStats e veja todos os seus gastos mensais e o histórico para ter certeza da sua economia!
+                </p>
+                <span>Experimente gratuitamente agora</span>
+                <button class="bg-zinc-800 text-white rounded-lg  px-3 py-1 ml-auto font-semibold">Entrar com o Google</button>
+            </div>
+            <figure class="py-10 w-5/12">
+                <img class="w-10/12 opacity-70 max-w-xl mr-20" src="../assets/img/graph.png" alt="">
+            </figure>
+        </section>
+    </main>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import Logo from "~~/components/Logo.vue";
 // import {loginPopUp} from '../services/firebase-functions'
 // export default {
 //     data(){
@@ -23,8 +41,7 @@
 //         loginPopUp()
 //     }
 
-
 // }
-const teste = ref(0)
-const login = ()=> loginPopUp()
+// const teste = ref(0)
+const login = () => loginPopUp();
 </script>
